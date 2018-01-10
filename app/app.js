815c13e0d35953a0c1123c84554ed3455dccce21
Waleed Ahmed <waleedahmed3045@gmail.com>
2018-01-10 15:41:59 -0500

    Included Routing

--- app/app.js
+++ app/app.js
@@ -1,5 +1,23 @@
 
-let app = angular.module("app", []);
+let app = angular.module("app", ['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+   $routeProvider
+
+       .when('/',{
+           templateUrl : 'templates/home.html'
+       })
+
+       .when('/resume',{
+           templateUrl: 'templates/resume.html'
+       })
+
+       .when('/contact',{
+           templateUrl: 'templates/contact.html'
+       });
+
+});
 
 app.controller('MainController',['$scope', function ($scope) {
 
