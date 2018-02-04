9829ea29351a07c76f8c08a774fcfa08703a7f4d
Waleed Ahmed <waleedahmed3045@gmail.com>
2018-02-04 09:50:44 -0500

    Changes single quotes to double. Added Alt tag for image

--- app/app.js
+++ app/app.js
@@ -1,26 +1,26 @@
 
-let app = angular.module("app", ['ngRoute']);
+let app = angular.module("app", ["ngRoute"]);
 
 
 app.config(function ($routeProvider) {
    $routeProvider
 
-       .when('/',{
-           templateUrl : 'templates/home.html'
+       .when("/",{
+           templateUrl : "templates/home.html"
        })
 
-       .when('/resume',{
-           templateUrl: 'templates/resume.html'
+       .when("/resume",{
+           templateUrl: "templates/resume.html"
        })
 
-       .when('/contact',{
-           templateUrl: 'templates/contact.html'
+       .when("/contact",{
+           templateUrl: "templates/contact.html"
        });
 
 });
 
-app.controller('MainController',['$scope', function ($scope) {
+app.controller("MainController",["$scope", function ($scope) {
 
-    $scope.name = 'Waleed Personal Website';
+    $scope.name = "Waleed Personal Website";
     
 }]);
